b75ede57599267f1b704f3d05cf32d3e0acdf1cf
jhuisa <huisaj30@gmail.com>
2024-05-25 16:10:03 -0500

    Adding database migration

---
+++ src/db/scripts/dbMigrate.ts
@@ -0,0 +1,42 @@
+import { config as configDotenv } from "dotenv";
+import path from "path";
+import fs from "fs";
+import { Umzug, JSONStorage } from "umzug";
+import { pool, query } from "../../config/dbConfig";
+
+if (process.env["NODE_ENV"] === "test") {
+  configDotenv({ path: ".env.test" });
+} else {
+  configDotenv();
+}
+
+const migrationsFileName =
+  process.env["NODE_ENV"] === "test"
+    ? "migrations.test.json"
+    : "migrations.json";
+
+const migrator = new Umzug({
+  migrations: { glob: path.join(__dirname, "../migrations/*.ts") },
+  context: { query },
+  storage: new JSONStorage({
+    path: path.join(__dirname, "../migrations", migrationsFileName),
+  }),
+  logger: console,
+  create: {
+    folder: path.join(__dirname, "../migrations"),
+    template: (filepath) => [
+      [
+        filepath,
+        fs
+          .readFileSync(
+            path.join(__dirname, "../template/migration-template.ts")
+          )
+          .toString(),
+      ],
+    ],
+  },
+});
+
+export type Migration = typeof migrator._types.migration;
+
+migrator.runAsCLI().then(() => pool.end());
