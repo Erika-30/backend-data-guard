8acbca3babac0e00996c1094d16020b0ba1b23df
jhuisa <huisaj30@gmail.com>
2024-05-25 16:02:02 -0500

    Adding user create user function and get user

---
+++ src/data/users.data.ts
@@ -0,0 +1,26 @@
+import { query } from "../config/dbConfig";
+import { User, UserData } from "../config/dbConfig";
+
+export async function createUserInDB(user: UserData): Promise<User> {
+  const queryText = `
+    INSERT INTO users (username, email, age, role, password)
+    VALUES ($1, $2, $3, $4, $5)
+    RETURNING *;
+  `;
+  const values: (string | number | boolean | null)[] = [
+    user.username,
+    user.email,
+    user.age == undefined ? null : user.age,
+    user.role,
+    user.password,
+  ];
+  const result = await query(queryText, values);
+  return result.rows[0];
+}
+
+export async function getUserByEmailFromDB(email: string): Promise<User> {
+  const queryText = "SELECT * FROM users WHERE email = $1";
+  const values = [email];
+  const result = await query(queryText, values);
+  return result.rows[0];
+}
