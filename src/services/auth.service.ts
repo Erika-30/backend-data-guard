9218ab00810ad5f032b9ee326a8c98abf95e5fa8
jhuisa <huisaj30@gmail.com>
2024-05-25 15:59:38 -0500

    Adding user service content

---
+++ src/services/auth.service.ts
@@ -0,0 +1,33 @@
+import { getUserByEmailFromDB, createUserInDB } from "../data/users.data";
+import bcrypt from "bcrypt";
+import { User, UserData } from "../config/dbConfig";
+
+export async function createUser(userData: UserData): Promise<User> {
+  const user = await getUserByEmailFromDB(userData.email);
+
+  if (user !== undefined) {
+    throw new Error("Email already in use");
+  }
+
+  userData.password = await bcrypt.hash(userData.password, 10);
+
+  return await createUserInDB(userData);
+}
+
+export async function validateUserCredentials(
+  email: string,
+  password: string
+): Promise<User> {
+  const user = await getUserByEmailFromDB(email);
+
+  if (!user) {
+    throw new Error("Usuario no encontrado");
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    throw new Error("Invalid password");
+  }
+
+  return user;
+}
