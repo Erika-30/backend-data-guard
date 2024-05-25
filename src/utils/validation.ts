371b64cb479821ecd45debee430d649be8aed66e
jhuisa <huisaj30@gmail.com>
2024-05-25 16:40:46 -0500

    adding user validation

---
+++ src/utils/validation.ts
@@ -0,0 +1,15 @@
+import { UserSchema } from "../config/dbConfig";
+
+export const validateUser = (user: any) => {
+  const result = UserSchema.safeParse(user);
+  if (!result.success) {
+    return {
+      success: false,
+      errors: result.error.errors.map((err) => ({
+        path: err.path.join("."),
+        message: err.message,
+      })),
+    };
+  }
+  return { success: true, data: result.data };
+};
