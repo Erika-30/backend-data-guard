c260c9e0befa088e2b88e9975729198ecc88babd
jhuisa <huisaj30@gmail.com>
2024-05-25 15:55:26 -0500

    Adding routers

---
+++ src/api/routes/uploadRoutes.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import multer from "multer";
+import { authMiddleware } from "../middlewares/authMiddleware";
+import { uploadCsv } from "../controllers/uploadController";
+
+const storage = multer.memoryStorage();
+const upload = multer({ storage: storage });
+
+const uploadRouter = express.Router();
+
+uploadRouter.post("/upload", authMiddleware, upload.single("file"), uploadCsv);
+
+export default uploadRouter;
