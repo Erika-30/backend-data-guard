bae33a0aace123db1e2cadd10797ec4efced49cd
jhuisa <huisaj30@gmail.com>
2024-05-25 15:58:04 -0500

    Adding controllers and user schema

---
+++ src/api/controllers/authController.ts
@@ -0,0 +1,57 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import {
+  createUser,
+  validateUserCredentials,
+} from "../../services/auth.service";
+import { UserSchema } from "../../config/dbConfig";
+
+const jwtSecret = process.env["JWT_SECRET"] || "your-secret-key";
+
+export const validateUser = (
+  req: Request,
+  _res: Response,
+  next: NextFunction
+) => {
+  const result = UserSchema.safeParse(req.body);
+  if (!result.success) {
+    return next({
+      status: 400,
+      message: "Validation Error",
+      details: result.error.errors,
+    });
+  }
+  req.body = result.data;
+  next();
+};
+
+export const signup = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const user = await createUser(req.body);
+    res
+      .status(201)
+      .json({ ok: true, message: "User created successfully", data: user });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const login = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { email, password } = req.body;
+    const user = await validateUserCredentials(email, password);
+    const payload = { user: user.id };
+    const token = jwt.sign(payload, jwtSecret, { expiresIn: "1h" });
+    res.json({ ok: true, message: "Login successful", token });
+  } catch (error) {
+    next(error);
+  }
+};
---
+++ src/config/dbConfig.ts
@@ -0,0 +1,79 @@
+import { config as configDotenv } from "dotenv";
+import { Pool } from "pg";
+import { z } from "zod";
+
+if (process.env["NODE_ENV"] === "test") {
+  configDotenv({ path: ".env.test" });
+} else {
+  configDotenv();
+}
+
+const { PGHOST, PGPORT, PGDATABASE, PGUSER, PGPASSWORD, PGADMINDATABASE } =
+  process.env;
+
+if (
+  !PGHOST ||
+  !PGPORT ||
+  !PGDATABASE ||
+  !PGUSER ||
+  !PGPASSWORD ||
+  !PGADMINDATABASE
+) {
+  throw new Error("Required environment variables are not defined");
+}
+
+export const pool = new Pool({
+  host: PGHOST,
+  port: Number(PGPORT),
+  database: PGDATABASE,
+  user: PGUSER,
+  password: PGPASSWORD,
+});
+
+export const query = async (
+  text: string,
+  params?: (string | number | boolean | null)[]
+) => {
+  try {
+    const results = await pool.query(text, params);
+    return results;
+  } catch (error) {
+    console.error(`Error executing query: ${text}`, error);
+    throw error;
+  }
+};
+
+export const UserSchema = z.object({
+  id: z.number().optional(),
+  username: z
+    .string()
+    .min(1, "El nombre es requerido")
+    .max(30, "El nombre no puede exceder los 30 caracteres")
+    .regex(/^[a-zA-Z ]*$/, "El nombre solo puede contener letras y espacios"),
+  email: z
+    .string()
+    .email("Debe ser un email válido")
+    .max(50, "El email no puede exceder los 50 caracteres"),
+  age: z
+    .string()
+    .refine((value) => /^\d+$/.test(value), {
+      message: "La edad debe ser un número positivo",
+      path: ["age"],
+    })
+    .optional()
+    .nullable(),
+  role: z
+    .string()
+    .refine((value) => /^user$|^admin$/.test(value), {
+      message: "Debe ser 'user' o 'admin'",
+    })
+    .default("user"),
+  createdAt: z.date().optional(),
+  updatedAt: z.date().optional(),
+  password: z.string().min(8, "La contraseña debe tener al menos 8 caracteres"),
+});
+
+export type User = z.infer<typeof UserSchema>;
+export type UserData = Omit<User, "id" | "createdAt" | "updatedAt"> & {
+  password?: string;
+};
